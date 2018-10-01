578bcb3a281d14ff26b1256767f0942a2fcfd86d
App-Code <sean2kay@gmail.com>
2018-10-01 11:19:54 +0000

    Shell script to pull down all the required images.

---
+++ pull-img.sh
@@ -0,0 +1,12 @@
+docker pull s34n/config-img
+docker pull s34n/eureka-img 
+docker pull s34n/game-img 
+docker pull s34n/leader-data-img 
+docker pull s34n/leaderboard-img 
+docker pull s34n/neo4j-img 
+docker pull s34n/rmq-img 
+docker pull s34n/sciroc-img 
+docker pull s34n/zipkin-img 
+docker pull s34n/zuul-img
+docker pull s34n/turbine-img
+docker images
