6c02cba1be9a145f0bcd93839e258bc1b39361f7
Sean Huni <sean2kay@gmail.com>
2020-06-28 22:59:46 +0200

    Updated the versions to 1.0.2

--- pull-img.sh
+++ pull-img.sh
@@ -1,12 +1,12 @@
 docker pull s34n/config-img:1.0.2 &&
-docker pull s34n/eureka-img:1.0.2 &&
-docker pull s34n/game-img:1.0.2 &&
-docker pull s34n/leader-data-img:1.0.2 &&
-docker pull s34n/leaderboard-img:1.0.2 &&
-docker pull s34n/sciroc-img:1.0.2 &&
-docker pull s34n/turbine-img:1.0.2 &&
-docker pull s34n/zuul-img:1.0.2 &&
-docker pull s34n/neo4j-img &&
-docker pull s34n/zipkin-img &&
-docker pull s34n/rmq-img &&
-docker images
+  docker pull s34n/eureka-img:1.0.2 &&
+  docker pull s34n/game-img:1.0.2 &&
+  docker pull s34n/leader-data-img:1.0.2 &&
+  docker pull s34n/leaderboard-img:1.0.2 &&
+  docker pull s34n/sciroc-img:1.0.2 &&
+  docker pull s34n/turbine-img:1.0.2 &&
+  docker pull s34n/zuul-img:1.0.2 &&
+  docker pull s34n/neo4j-img &&
+  docker pull s34n/zipkin-img &&
+  docker pull s34n/rmq-img &&
+  docker images
