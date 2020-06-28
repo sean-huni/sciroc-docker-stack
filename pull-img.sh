a505138c1839686e22d017c5971f4b8ed10d61ec
Sean Huni <sean2kay@gmail.com>
2020-06-29 00:26:01 +0200

    Updated the versions to 1.0.3

--- pull-img.sh
+++ pull-img.sh
@@ -1,7 +1,7 @@
 docker pull s34n/config-img:1.0.2 &&
-  docker pull s34n/eureka-img:1.0.2 &&
+  docker pull s34n/eureka-img:1.0.3 &&
   docker pull s34n/game-img:1.0.2 &&
-  docker pull s34n/leader-data-img:1.0.2 &&
+  docker pull s34n/leader-data-img:1.0.3 &&
   docker pull s34n/leaderboard-img:1.0.2 &&
   docker pull s34n/sciroc-img:1.0.2 &&
   docker pull s34n/turbine-img:1.0.2 &&
